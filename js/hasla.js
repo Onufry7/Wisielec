2066d9ba573e29c07b5ef9b866da8d49d51c4d49
Szymon <onufry7@gmail.com>
2019-01-17 00:56:37 +0100

    Pierwsze repozytorium
    Utworzenie pierwszeggo repozytorium po zmianach językowych.

---
+++ js/hasla.js
@@ -0,0 +1,360 @@
+//Tablica z hasłami
+var hasla =  new Array();
+
+//Przysłowia
+hasla[0] = 
+{
+	K1:"Przysłowie",
+	PL:"Bez pracy nie ma kołaczy",
+	K2:"Adage",
+	EN:"No pain, no gain"
+};
+hasla[1] = 
+{
+	K1:"Przysłowie",
+	PL:"Zła wymówka jest lepsza niż żadna",
+	K2:"Adage",
+	EN:"A bad excuse is better than none"
+};
+hasla[2] = 
+{
+	K1:"Przysłowie",
+	PL:"Kiepskiej baletnicy przeszkadza rąbek u spódnicy",
+	K2:"Adage",
+	EN:"A bad workman blames his tools"
+};
+hasla[3] = 
+{
+	K1:"Przysłowie",
+	PL:"Lepiej późno niż wcale",
+	K2:"Adage",
+	EN:"Better late than never"
+};
+hasla[4] = 
+{
+	K1:"Przysłowie",
+	PL:"Głupie pytanie, głupia odpowiedź",
+	K2:"Adage",
+	EN:"Ask a silly question and you get a silly answer"
+};
+hasla[5] = 
+{
+	K1:"Przysłowie",
+	PL:"Nie szata zdobi człowieka",
+	K2:"Adage",
+	EN:"Beauty is only skin deep"
+};
+hasla[6] = 
+{
+	K1:"Przysłowie",
+	PL:"Cicha woda brzegi rwie",
+	K2:"Adage",
+	EN:"Constant dropping wears away a stone"
+};
+hasla[7] = 
+{
+	K1:"Przysłowie",
+	PL:"Ciekawość to pierwszy stopień do piekła",
+	K2:"Adage",
+	EN:"Curiosity killed the cat"
+};
+hasla[8] = 
+{
+	K1:"Przysłowie",
+	PL:"Nie mów hop póki nie przeskoczysz",
+	K2:"Adage",
+	EN:"Don't count your chickens before they're hatched"
+};
+hasla[9] = 
+{
+	K1:"Przysłowie",
+	PL:"Nie stawiaj wszystkiego na jedną kartę",
+	K2:"Adage",
+	EN:"Don't put all your eggs in one basket"
+};
+hasla[10] = 
+{
+	K1:"Przysłowie",
+	PL:"Nie rób z igły widły",
+	K2:"Adage",
+	EN:"Don't make a mountain out of a molehill"
+};
+hasla[11] = 
+{
+	K1:"Przysłowie",
+	PL:"Cel uświęca środki",
+	K2:"Adage",
+	EN:"The end justifies the means"
+};
+hasla[12] = 
+{
+	K1:"Przysłowie",
+	PL:"Nie ma sensu płakać nad rozlanym mlekiem",
+	K2:"Adage",
+	EN:"It is no use crying over spilt milk"
+};
+hasla[13] = 
+{
+	K1:"Przysłowie",
+	PL:"Apetyt rośnie w miarę jedzenia",
+	K2:"Adage",
+	EN:"The appetite grows with what it feeds on"
+};
+hasla[14] = 
+{
+	K1:"Przysłowie",
+	PL:"Chciwy dwa razy traci",
+	K2:"Adage",
+	EN:"Grasp all, lose all"
+};
+hasla[15] = 
+{
+	K1:"Przysłowie",
+	PL:"Ćwiczenie prowadzi do doskonałości",
+	K2:"Adage",
+	EN:"Use makes mastery"
+};
+hasla[16] = 
+{
+	K1:"Przysłowie",
+	PL:"Fortuna kołem się toczy",
+	K2:"Adage",
+	EN:"Fortune is fickle"
+};
+hasla[17] = 
+{
+	K1:"Przysłowie",
+	PL:"Wiara przenosi góry",
+	K2:"Adage",
+	EN:"Faith will move mountains"
+};
+hasla[18] = 
+{
+	K1:"Przysłowie",
+	PL:"Są gusta i guściki",
+	K2:"Adage",
+	EN:"There is no accounting for tastes"
+};
+hasla[19] = 
+{
+	K1:"Przysłowie",
+	PL:"Prawdziwych przyjaciół poznaje się w biedzie",
+	K2:"Adage",
+	EN:"A friend in need is a friend indeed"
+};
+hasla[20] = 
+{
+	K1:"Przysłowie",
+	PL:"Kto się na gorącym sparzył, ten dmucha na zimne",
+	K2:"Adage",
+	EN:"Once bitten, twice shy"
+};
+hasla[21] = 
+{
+	K1:"Przysłowie",
+	PL:"Kuj żelazo póki gorące",
+	K2:"Adage",
+	EN:"Make hay while the sun shines"
+};
+hasla[22] = 
+{
+	K1:"Przysłowie",
+	PL:"Kto rano wstaje, temu Pan Bóg daje",
+	K2:"Adage",
+	EN:"The early bird catches the worm"
+};
+hasla[23] = 
+{
+	K1:"Przysłowie",
+	PL:"Czas leczy rany",
+	K2:"Adage",
+	EN:"Time is a great healer"
+};
+hasla[24] = 
+{
+	K1:"Przysłowie",
+	PL:"Lepszy rydz niż nic",
+	K2:"Adage",
+	EN:"Half a loaf is better than none"
+};
+
+//Tytuły filmów
+hasla[25] = 
+{
+	K1:"Film",
+	PL:"Władca Pierścieni Powrót króla",
+	K2:"Movie",
+	EN:"The Lord of the Rings The Return of the King"
+};
+hasla[26] = 
+{
+	K1:"Film",
+	PL:"Podziemny krąg",
+	K2:"Movie",
+	EN:"Fight Club"
+};
+hasla[27] = 
+{
+	K1:"Film",
+	PL:"Chłopcy z ferajny",
+	K2:"Movie",
+	EN:"Goodfellas"
+};
+hasla[28] = 
+{
+	K1:"Film",
+	PL:"Piękny umysł",
+	K2:"Movie",
+	EN:"A Beautiful Mind"
+};
+hasla[29] = 
+{
+	K1:"Film",
+	PL:"Incepcja",
+	K2:"Movie",
+	EN:"Inception"
+};
+hasla[30] = 
+{
+	K1:"Film",
+	PL:"Milczenie owiec",
+	K2:"Movie",
+	EN:"The Silence of the Lambs"
+};
+hasla[31] = 
+{
+	K1:"Film",
+	PL:"Django",
+	K2:"Movie",
+	EN:"Django Unchained"
+};
+hasla[32] = 
+{
+	K1:"Film",
+	PL:"Król Lew",
+	K2:"Movie",
+	EN:"The Lion King"
+};hasla[33] = 
+{
+	K1:"Film",
+	PL:"Człowiek z blizną",
+	K2:"Movie",
+	EN:"Scarface"
+};
+hasla[34] = 
+{
+	K1:"Film",
+	PL:"Wyspa tajemnic",
+	K2:"Movie",
+	EN:"Shutter Island"
+};
+hasla[35] = 
+{
+	K1:"Film",
+	PL:"Buntownik z wyboru",
+	K2:"Movie",
+	EN:"Good Will Hunting"
+};hasla[36] = 
+{
+	K1:"Film",
+	PL:"Gwiezdne wojny Powrót Jedi",
+	K2:"Movie",
+	EN:"Star Wars Return of the Jedi"
+};
+hasla[37] = 
+{
+	K1:"Film",
+	PL:"W pogoni za szczęściem",
+	K2:"Movie",
+	EN:"The Pursuit of Happyness"
+};
+hasla[38] = 
+{
+	K1:"Film",
+	PL:"Mroczny Rycerz",
+	K2:"Movie",
+	EN:"The Dark Knight"
+};hasla[39] = 
+{
+	K1:"Film",
+	PL:"Infiltracja",
+	K2:"Movie",
+	EN:"The Departed"
+};
+hasla[40] = 
+{
+	K1:"Film",
+	PL:"Siedem dusz",
+	K2:"Movie",
+	EN:"Seven Pounds"
+};
+hasla[41] = 
+{
+	K1:"Film",
+	PL:"Łowca jeleni",
+	K2:"Movie",
+	EN:"The Deer Hunter"
+};hasla[42] = 
+{
+	K1:"Film",
+	PL:"Bękarty wojny",
+	K2:"Movie",
+	EN:"Inglourious Basterds"
+};
+hasla[43] = 
+{
+	K1:"Film",
+	PL:"Ojciec chrzestny",
+	K2:"Movie",
+	EN:"The Godfather"
+};
+hasla[44] = 
+{
+	K1:"Film",
+	PL:"Zwierzogród",
+	K2:"Movie",
+	EN:"Zootopia"
+};hasla[45] = 
+{
+	K1:"Film",
+	PL:"Złap mnie, jeśli potrafisz",
+	K2:"Movie",
+	EN:"Catch Me If You Can"
+};
+hasla[46] = 
+{
+	K1:"Film",
+	PL:"Pół żartem, pół serio",
+	K2:"Movie",
+	EN:"Some Like It Hot"
+};
+hasla[47] = 
+{
+	K1:"Film",
+	PL:"Helikopter w ogniu",
+	K2:"Movie",
+	EN:"Black Hawk Down"
+};
+hasla[48] = 
+{
+	K1:"Film",
+	PL:"Śniadanie u Tiffanyego",
+	K2:"Movie",
+	EN:"Breakfast at Tiffanys"
+};
+hasla[49] = 
+{
+	K1:"Film",
+	PL:"Choć goni nas czas",
+	K2:"Movie",
+	EN:"The Bucket List"
+};
+
+//#####
+/* hasla[50] = 
+{
+	K1:"Hasło",
+	PL:"Polski",
+	K2:"Sentence",
+	EN:"Angielski"
+}; */
---
+++ js/szubienica.js
@@ -0,0 +1,228 @@
+//Dzwięki gry
+var tak = new Audio("sound/yes.wav");
+var nie = new Audio("sound/no.wav");
+var poczatek = new Audio("sound/start.wav");
+var koniec = new Audio("sound/end.wav");
+var dzwiek = false; 
+//Startowe wartości zmiennych
+var jezykGry = "PL";
+var liczbaSkuch = 0; 
+//Ustawienie hasła
+var nrHasla = Math.ceil(Math.random()*hasla.length-1); //Losowanie hasła
+var hasloPL = hasla[nrHasla]['PL'].toUpperCase();
+var hasloEN = hasla[nrHasla]['EN'].toUpperCase();
+var kategoria = ""; //Kategoria hasła
+var haslo = ""; //Hasło
+var haslo1 = ""; // Wykreskowane hasło
+var alfabet = Array();
+
+
+
+function generujHaslo()
+{
+	//Pobiera hasła
+	if(jezykGry == "PL") 
+	{
+		haslo = hasloPL;
+		kategoria = hasla[nrHasla]['K1'];
+	}
+	else if(jezykGry == "EN") 
+	{
+		haslo = hasloEN
+		kategoria = hasla[nrHasla]['K2'];
+	}
+	
+	//Wykreskowanie hasła
+	for(i=0; i<haslo.length; i++)
+	{
+		if(haslo.charAt(i) == " ") haslo1 = haslo1 + " ";
+		else if(haslo.charAt(i) == ",") haslo1 = haslo1 + ",";
+		else if(haslo.charAt(i) == ".") haslo1 = haslo1 + ".";
+		else haslo1 = haslo1 + "_";
+	}	
+
+	//Odkreskowanie hasla
+	String.prototype.ustawZnak = function(miejsce, znak)
+	{
+		if(miejsce > this.length-1) return this.toString();
+		else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
+	}
+}
+
+
+
+//Dzwięki gry
+function dzwieki()
+{
+	if(dzwiek == false) 
+	{
+		dzwiek = true;
+		document.getElementById("dzwiek").innerHTML = "&#128266;";
+	}
+	else
+	{
+		dzwiek = false;
+		document.getElementById("dzwiek").innerHTML = "&#128263;";
+	}		
+}
+
+
+
+//Wypisanie hasla1 i kategorii
+function wypiszHaslo()
+{
+	document.getElementById("kategoria").innerHTML = kategoria;
+	document.getElementById("haslo").innerHTML = haslo1;
+}
+
+
+
+//Sprawdza litery w chaśle
+function sprawdz(nr)
+{
+	var trafiona = false;
+	var nrId = "lit" + nr;
+	
+	//Sprawdza czy trafiliśmy
+	for(i=0; i<haslo.length; i++)
+	{
+		if(haslo.charAt(i) == alfabet[nr])
+		{
+			haslo1 = haslo1.ustawZnak(i, alfabet[nr]);
+			trafiona = true;
+		}
+	}
+	
+	//Obsługa trafienia i błedu
+	if(trafiona == true)
+	{
+		document.getElementById(nrId).classList.add("poprawna");
+		if(dzwiek == true) tak.play();
+		wypiszHaslo();
+	}
+	else
+	{
+		liczbaSkuch++; //Skuchy
+		var obraz = "img/s"+liczbaSkuch+".png";
+		document.getElementById(nrId).classList.add("bledna");
+		if(dzwiek == true) nie.play();
+		document.getElementById("obrazek").src = obraz;
+		document.getElementById("szanse").innerHTML = 9-liczbaSkuch;
+	}
+	
+	//Usunięcie onclicka z wykorzystanego przycisku
+	document.getElementById(nrId).removeAttribute("onclick");
+	
+	//Wygrana
+	if(haslo == haslo1) koniecGry(1,poczatek);
+
+	//Przegrana
+	if(liczbaSkuch >= 9) koniecGry(0,koniec);
+}
+
+
+
+function wyswietlAlfabet()
+{
+	if(jezykGry == "PL") alfabet = alfabetPL;
+	else if(jezykGry == "EN") alfabet = alfabetEN;
+	var trescDiva = "";
+	
+	//Generowanie alfabetu
+	for(i=0; i<alfabet.length; i++)
+	{
+		var element = "lit" + i;
+		trescDiva = trescDiva + '<div class="litera" id="' + element + '" onclick = "sprawdz(' + i + ')">' + alfabet[i] + '</div>';
+	}
+	//Wyświetlenie alfabetu
+	document.getElementById("alfabet").innerHTML = trescDiva;
+}
+
+
+
+function koniecGry(wynik, melodia)
+{
+	//Przygotowuje i wyświetla komunikat na koniec gry
+	var jeszczeRaz = (jezykGry == "EN") ? "TRY AGAIN ?" : "JESZCZE RAZ ?";
+	var dobreHaslo = (jezykGry == "EN") ? "The correct password:" : "Prawidłowe hasło:";
+	
+	switch(wynik)
+	{
+		case 1: wynik = (jezykGry == "EN") ? "You Win!" : "Wygrywasz!"; break;
+		case 0: wynik = (jezykGry == "EN") ? "You Lose!" : "Przegrywasz!"; break;
+	}
+
+	var hasla = '<p id="pl-haslo">'+hasloPL+'</p><p id="en-haslo">'+hasloEN+'</p>';
+	var odNowa = '<span class="reset" id="nowa">'+jeszczeRaz+'</span>';
+	
+	document.getElementById("alfabet").innerHTML ="<p>"+wynik+"</p><p>"+dobreHaslo+"</p>"+hasla+odNowa;
+	
+	//Dodaje obsługe kliknięcia przycisku "jeszcze raz"
+	document.getElementById("nowa").addEventListener("click", function(){location.reload();}); 
+	
+	//Odtwarza dzwięk dla końca gry
+	if(dzwiek == true) melodia.play(); 
+	
+	//Ukkrycie przycisku "nowa gra"
+	document.getElementById("reset").style.visibility = "hidden";
+}
+
+
+
+function przygotujPlansze()
+{
+	//Załadowanie grafiki
+	document.getElementById("grafika").innerHTML = '<img src="img/s0.png" alt="" id="obrazek">';
+	
+	//Ustawia komunikaty menu
+	var reset = (jezykGry == "EN") ? "NEW GAME" : "NOWA GRA";
+	var szanse = (jezykGry == "EN") ? "Left over chances." : "Pozostałe szanse.";
+	var info = (jezykGry == "EN") ? "Try to guess the password before the flower fades." : "Spróbuj odgadnąć hasło zanim kwiatek zwiędnie.";
+	var dzwiek = (jezykGry == "EN") ? "Sounds game" : "Dzwięki gry.";
+	
+	//Ustawienie elementów menu gry
+	document.getElementById("szanse").innerHTML = 9;
+	document.getElementById("reset").innerHTML = reset;
+	document.getElementById("tSzanse").setAttribute("data-tooltip", szanse);
+	document.getElementById("tInfo").setAttribute("data-tooltip", info);
+	document.getElementById("tDzwiek").setAttribute("data-tooltip", dzwiek);
+	document.getElementById("info").style.visibility = "visible";
+	document.getElementById("menu").style.visibility = "visible";
+}
+
+
+
+function rozpocznij()
+{
+	przygotujPlansze();
+	generujHaslo();
+	wypiszHaslo();
+	wyswietlAlfabet();
+}
+
+
+
+function start()
+{
+	//Początkowe elementy gry - wybór języka 
+	document.getElementById("kategoria").innerHTML = 'Wybierz język / Choose language<br>';
+	document.getElementById("kategoria").innerHTML += '<img src="img/pl.png" alt="PL" id="PL" class="jezyk">';
+	document.getElementById("kategoria").innerHTML += '<img src="img/en.png" alt="EN" id="EN" class="jezyk">';
+	
+	//Obsługa kliknięć przycisków: "nowa gra" i "dzwięki gry"
+	document.getElementById("reset").addEventListener("click", function(){location.reload();});
+	document.getElementById("dzwiek").addEventListener("click", dzwieki);
+	
+	//Zapisanie wybranego języka
+	document.getElementById("EN").addEventListener("click", function() {jezykGry = 'EN';});
+	document.getElementById("PL").addEventListener("click", function() {jezykGry = 'PL';});
+	
+	//Rozpoczęcie gry
+	document.getElementById("EN").addEventListener("click", rozpocznij);
+	document.getElementById("PL").addEventListener("click", rozpocznij);
+}
+
+
+
+//Uruchomienie skryptu przy starcie
+window.onload = start;
